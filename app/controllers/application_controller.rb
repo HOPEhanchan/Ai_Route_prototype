88d655b0a8d6d191d4e3d4c6ff0f09bbcef81956
半田望 <_hope_@handanoomunoAir.lan>
2025-11-27 04:16:31 +0900

    ログイン / ログアウト / 新規登録画面のcss調整 + i18n微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!
   allow_browser versions: :modern
+
+  private
+
+  # ログイン後の遷移先
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  # ログアウト後の遷移先
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
