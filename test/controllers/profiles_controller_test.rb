19d5802761c907c509029bf439a8c7edb5751d48
半田望 <_hope_@handanoomunoAir.lan>
2025-11-30 17:23:40 +0900

    Add user profile fields and profile pages

---
+++ test/controllers/profiles_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  test 'profile controller will be tested later' do
+    skip 'Profile のテストは後続Issueで実装予定'
+  end
+end
