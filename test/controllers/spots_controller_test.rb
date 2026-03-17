330863db5c00e6660d1e48f138f15a3743f1d78a
半田望 <_hope_@handanoomunoAir.lan>
2026-03-17 18:19:41 +0900

    test: add spots controller tests

--- test/controllers/spots_controller_test.rb
+++ test/controllers/spots_controller_test.rb
@@ -3,7 +3,54 @@
 require 'test_helper'
 
 class SpotsControllerTest < ActionDispatch::IntegrationTest
-  test 'spots controller will be tested later' do
-    skip 'Devise 認証付きでのテストは後続Issueで実装予定'
+  fixtures :users, :tags, :spots, :taggings
+
+  test 'should redirect new when not logged in' do
+    get new_spot_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'should get new when logged in' do
+    sign_in users(:one)
+    get new_spot_path
+    assert_response :success
+  end
+
+  test 'should create spot with valid params when logged in' do
+    sign_in users(:one)
+
+    SpotMetadataFetcher.stub(:call, {
+      title: '取得タイトル',
+      description: '取得説明',
+      image_url: 'https://example.com/image.jpg'
+    }) do
+      assert_difference('Spot.count', 1) do
+        post spots_path, params: {
+          spot: {
+            title: '新しいスポット',
+            url: 'https://example.com/new-spot',
+            description: '説明文',
+            memo: 'メモ'
+          }
+        }
+      end
+    end
+
+    assert_redirected_to spots_path
+  end
+
+  test 'should not create spot with invalid params' do
+    sign_in users(:one)
+
+    assert_no_difference('Spot.count') do
+      post spots_path, params: {
+        spot: {
+          title: '',
+          url: ''
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,15 +3,22 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/mock'
 
-module ActiveSupport
-  class TestCase
+
+class ActiveSupport::TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    # fixtures :all ← Continuous Integrationのエラー回避のためコメントアウト
+    # fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
+
+class Application
+    # Add more helper methods to be used by all tests here...
+  end
+
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 end
