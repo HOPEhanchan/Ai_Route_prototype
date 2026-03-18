30c9bd872202c0181ce836e20e08d6deef6bbc9c
半田望 <_hope_@handanozomunoMacBook-Air.local>
2026-03-18 14:39:41 +0900

    自動テストの実装: lists + profiles

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
-  test 'profile controller will be tested later' do
-    skip 'Profile のテストは後続Issueで実装予定'
+  fixtures :users
+
+  test 'redirects to login when not authenticated' do
+    get profile_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'shows profile when logged in' do
+    sign_in users(:one)
+    get profile_url
+    assert_response :success
+  end
+
+  test 'updates profile with valid params' do
+    sign_in users(:one)
+
+    patch profile_url, params: {
+      user: { name: '更新後の名前' }
+    }
+
+    assert_redirected_to profile_path
+    assert_equal '更新後の名前', users(:one).reload.name
   end
 end
